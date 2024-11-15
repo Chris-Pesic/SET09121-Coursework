3a2c55097dfd076a75b586650a2419d6689c2fbe
Christian Pesic <40593740@live.napier.ac.uk>
2024-11-15 17:57:05 +0000

    frame rate formatting fixed

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -26,7 +26,7 @@ int hangTime = 0;
 Font font;
 Text text;
 int loops = 0;
-float fps = 0.0f;
+int fps = 0.0f;
 Time elapsedTime;
 
 
@@ -72,8 +72,6 @@ void Update(RenderWindow &window) {
         return;
     }
   }
-
-
   
   // Reset Ball falling speed
   //ballVelocity = { 0, initialVelocityY };
@@ -184,9 +182,9 @@ int main() {
         }
 
         if (elapsedTime.asSeconds() > 0.0f) {
-            fps = static_cast<float>(loops) / elapsedTime.asSeconds();
+            fps = loops / elapsedTime.asSeconds();
         }
-        String str_dt = "Delta Time: " + to_string(dt) + "\nFPS: " + to_string(fps);
+        String str_dt = "FPS: " + to_string(fps);
         // Update Score Text
         text.setString(str_dt);
         // Keep Score Text Centered
