300c0bb4bda171714e0c26180906d5fa4459fd72
Christian Pesic <40593740@live.napier.ac.uk>
2024-11-15 21:51:09 +0000

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -126,7 +126,7 @@ void Update(RenderWindow &window) {
   if (by > gameHeight - ballRadius) { //bottom wall
       // bottom wall
       // ballVelocity.x *= velocityMultiplier;
-      ballVelocity.y == 0.f;
+      ballVelocity.y *= 0.f;
       // ball.move(Vector2f(0.f, -10.f));
       canJump = true;
   }
@@ -163,7 +163,6 @@ int main() {
 
     window.setFramerateLimit(60);
     Clock clock;
-    Time timeSinceStart = clock.restart();
 
     while (window.isOpen()) {
 
@@ -186,13 +185,13 @@ int main() {
             fps = loops / elapsedTime.asSeconds();
         }
 
-        if (timeSinceStart.asSeconds() % 50 == 0){
-            String str_fps = "FPS: " + to_string(fps);
-            // Update Score Text
-            text.setString(str_fps);
-            // Keep Score Text Centered
-            text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
-        }
+        String str_fps = to_string(fps);
+
+        String str_Header = "FPS: " + str_fps;
+        // Update Score Text
+        text.setString(str_Header);
+        // Keep Score Text Centered
+        text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
 
         window.clear();
         Update(window);
