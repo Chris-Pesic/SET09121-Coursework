404cc6315786404c9397bf9253dc2df594f663ef
Christian Pesic <40593740@live.napier.ac.uk>
2024-11-15 17:50:43 +0000

    frame rate reduced to 60fps, need to work on collision, ball keeps clipping into the floor

---
+++ Coursework/main.cpp
@@ -0,0 +1,202 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+void Reset();
+
+const Keyboard::Key controls[3] = {
+    Keyboard::W,   // Player1 Up
+    Keyboard::A,   // Player1 Left
+    Keyboard::D,  // Player1 Right
+};
+
+const Vector2f platformSize(800.f, 100.f);
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float ballRadius = 10.f;
+Vector2f ballVelocity;
+const float ballHorizontalSpeed = 600.f;
+const float ballJumpSpeed = -1000.f;
+const float initialVelocityY = 100.f;
+bool canJump = false;
+float dt;
+int jumpTime = 0;
+int hangTime = 0;
+Font font;
+Text text;
+int loops = 0;
+float fps = 0.0f;
+Time elapsedTime;
+
+
+CircleShape ball;
+RectangleShape platform[1];
+
+
+void Load() {
+
+    // Set size and origin of platform
+    //for (auto &p : platform) {
+      //p.setSize(platformSize);
+      //p.setOrigin(platformSize / 2.f);
+
+
+    // Set size and origin of ball
+    ball.setRadius(ballRadius);
+    ball.setOrigin(ballRadius, ballRadius); //Should be half the ball width and height
+
+    // Reset positions
+    //platform[0].setPosition(Vector2f(platformSize.x / 2.f, gameHeight / 12.f));
+    ball.setPosition(Vector2f(gameWidth / 2.f, gameHeight / 2.f));
+
+    // Set Ball falling speed
+    ballVelocity = { 0, initialVelocityY };
+
+
+
+
+}
+
+
+void Update(RenderWindow &window) {
+
+  // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+  // check and consume events
+  Event event;
+  while (window.pollEvent(event)) {
+    if (event.type == Event::Closed) {
+        window.close();
+        return;
+    }
+  }
+
+
+  
+  // Reset Ball falling speed
+  //ballVelocity = { 0, initialVelocityY };
+
+
+      //if (ballVelocity.y < 1000.f) {
+          //ballVelocity = { 0.f, ballVelocity.y + 1 };
+      //}
+
+  if (ballVelocity.y > -100.f && ballVelocity.y < 100.f) {
+      //kill time
+      hangTime++;
+      if (hangTime % 2 == 0) {
+          ballVelocity = { 0.f, ballVelocity.y + 100 };
+      }
+  }
+  else if (ballVelocity.y < 1000.f) {
+      ballVelocity = { 0.f, ballVelocity.y + 100 };
+  }
+
+
+
+
+  // Reset Jump validity
+  canJump = false;
+
+
+  //ball.move(ballVelocity * dt);
+
+  // Quit Via ESC Key
+  if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+      window.close();
+  }
+
+  // handle ball movement (horizontal)
+  float direction = 0.0f;
+  if (Keyboard::isKeyPressed(controls[1])) {
+      direction--;
+  }
+  if (Keyboard::isKeyPressed(controls[2])) {
+      direction++;
+  }
+  ball.move(Vector2f(direction * ballHorizontalSpeed * dt, 0.f));
+
+
+
+  // Check Collision with platform
+
+  const float bx = ball.getPosition().x;
+  const float by = ball.getPosition().y;
+
+  if (by > gameHeight - 16) { //bottom wall
+      // bottom wall
+      // ballVelocity.x *= velocityMultiplier;
+      ballVelocity.y *= 0.f;
+      // ball.move(Vector2f(0.f, -10.f));
+      canJump = true;
+  }
+  else if (by < 0) { //top wall
+      // top wall
+      // ballVelocity.x *= velocityMultiplier;
+      //ballVelocity.y *= 0;
+      ball.move(Vector2f(0.f, 10.f));
+  }
+
+  // handle ball jump
+  if (canJump == true) {
+      if (Keyboard::isKeyPressed(controls[0])) {
+          //ball.move(Vector2f(0.f, ballJumpSpeed * dt));
+          ballVelocity = { 0.f, ballJumpSpeed };
+          jumpTime = 10;
+      }
+  }
+
+  ball.move(ballVelocity * dt);
+  
+}
+
+void Render(RenderWindow &window) {
+    // Draw Everything
+    window.draw(platform[0]);
+    window.draw(ball);
+    window.draw(text);
+}
+
+int main() {
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "EGG WITH LEGG");
+    Load();
+
+    window.setFramerateLimit(60);
+    Clock clock;
+
+    while (window.isOpen()) {
+
+        fps = 0.f;
+        loops = 0;
+        // Load font-face from res dir
+        font.loadFromFile("C:/Users/chris/ENU OneDrive/OneDrive - Edinburgh Napier University/Year 3/Modules/TR1/Games Engineering/Coursework/SET09121-Coursework/res/fonts/RobotoMono-Regular.ttf");
+        // Set text element to use font
+        text.setFont(font);
+        // set the character size to 24 pixels
+        text.setCharacterSize(24);
+
+        elapsedTime = clock.restart();
+
+        if (elapsedTime.asSeconds() < 1) {
+            loops++;
+        }
+
+        if (elapsedTime.asSeconds() > 0.0f) {
+            fps = static_cast<float>(loops) / elapsedTime.asSeconds();
+        }
+        String str_dt = "Delta Time: " + to_string(dt) + "\nFPS: " + to_string(fps);
+        // Update Score Text
+        text.setString(str_dt);
+        // Keep Score Text Centered
+        text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
+
+        window.clear();
+        Update(window);
+        Render(window);
+        window.display();
+        loops++;
+    }
+    return 0;
+}
